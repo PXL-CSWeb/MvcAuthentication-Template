d641445e2978bc3a49425dcce07b7e906b5533ce
Kristof Palmaers <20007064@pxl.be>
2024-02-27 17:58:01 +0100

    Initial commit

---
+++ MVCAuthentication/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MVCAuthentication.ViewModels;
+
+namespace MVCAuthentication.Controllers
+{
+    
+    public class AccountController : Controller
+    {
+        UserManager<IdentityUser> _userManager;
+        SignInManager<IdentityUser> _signInManager;
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        #region Register
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterAsync(UserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = new IdentityUser();
+                user.UserName = model.UserName;
+                user.Email = model.Email;
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, Roles.USER_ROLE);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                        ModelState.AddModelError("", error.Description);
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+        #endregion
+        #region Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync(UserViewModel model)
+        {            
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, false, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    var searchUser = await _userManager.FindByNameAsync(model.UserName);
+                    if (searchUser != null)
+                        await _signInManager.SignInAsync(searchUser, isPersistent: false);
+                    return RedirectToAction("Index", "Test");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid login attempt");
+                }
+            }
+            return View("Login", model);
+        }
+        #endregion
+
+    }
+}
